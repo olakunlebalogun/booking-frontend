b10b4572a7de826bc71898ef948025456dd4945b
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-10-07 10:45:11 +0100

    changed some css properties

--- src/pages/ForgetPassword.js
+++ src/pages/ForgetPassword.js
@@ -11,24 +11,25 @@ const ForgetPassword = () => {
   }
 
   return (
-    <div  className='section'>
-    <div className='section-center' >
-      <div className='container' >
-        <div className='row'>
-          <div className='forgetpassword-form'>
-            <div className='form-header'id="header">
-              <h1>Reset Password</h1>
-              <p className=''>
-                Enter the email address associated with your account and we will send you a link to reset your password
-              </p>
-            </div>
-           
-            <form>
-              <div className='form-group'>
-                <span className='form-label'>Email</span>
-                <input className='form-control' type='email' />
+    <div className='section bg-dark'>
+      <div className='section-center'>
+        <div className='container'>
+          <div className='row'>
+            <div className='forgetpassword-form'>
+              <div className='form-header' id='header'>
+                <h1>Reset Password</h1>
+                <p className='sub-heading'>
+                  Enter the email address associated with your account and we
+                  will send you a link to reset your password
+                </p>
               </div>
-              {/* <div className='form-check mb-3'>
+
+              <form>
+                <div className='form-group'>
+                  <span className='form-label'>Email</span>
+                  <input className='form-control' type='email' />
+                </div>
+                {/* <div className='form-check mb-3'>
                     <input
                       className='form-check-input'
                       id='inputRememberPassword'
@@ -42,7 +43,7 @@ const ForgetPassword = () => {
                       Remember Password
                     </label>
                   </div> */}
-                  {/* <div className='d-flex align-items-center justify-content-center mt-4 mb-0'>
+                {/* <div className='d-flex align-items-center justify-content-center mt-4 mb-0'>
                     <Link to='/forgot-password' className='small'>
                       Forgot Password?
                     </Link>
@@ -53,35 +54,31 @@ const ForgetPassword = () => {
                       Continue
                     </button>
                   </div> */}
-                    <div className='d-flex align-items-center justify-content-between mt-4 mb-0'>
-                          {/* <a className='small' href='login.html'>
+                <div className='d-flex align-items-center justify-content-between mt-4 mb-0'>
+                  {/* <a className='small' href='login.html'>
                             Return to login
                           </a> */}
-                          <Link to='/login' className='small'>
-                            Return to login
-                          </Link>
-                          <button
-                            className='btn btn-primary'
-                            onClick={() => onResetPassword()}
-                          >
-                            Reset Password
-                          </button>
-                        </div>
-                        <div className='card-footer text-center py-3'>
-                      <div className='small'>
-                        
-                        <Link to='/signup'>
-                          Need an account? Sign up!
-                        </Link>
-                      </div>
-                    </div>
-            </form>
+                  <Link to='/login' className='small'>
+                    Return to login
+                  </Link>
+                  <button
+                    className='btn btn-primary'
+                    onClick={() => onResetPassword()}
+                  >
+                    Reset Password
+                  </button>
+                </div>
+                <div className='card-footer text-center py-3'>
+                  <div className='small'>
+                    <Link to='/signup'>Need an account? Sign up!</Link>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
-          
         </div>
       </div>
     </div>
-  </div>
   )
 }
 
