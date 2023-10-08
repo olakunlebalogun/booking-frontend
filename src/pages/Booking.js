dec06445ad9fac45055d73203d9bbfaf55f0d98c
larry659 <larryfunmbi26@gmail.com>
2023-10-08 15:15:14 +0100

    booking and login

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 // import api from './../api/data';
 
 
 import './../assets/css/pages/Booking.css'
 
 const Booking = () => {
+  const [data, setData] = useState({
+    email: "",
+    phone: "",
+    appointmentDate:"",
+    service:""
+  });
+ const [error, setError] = useState("")
 
+  //const{firstName,lastName,email,password,userName,phone}=signUp;
+  const onInputChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+
+  const navigate = useNavigate()
+  const onAddBooking = async (e) => {
+    e.preventDefault();
+    console.log(data)
+    const a = await axios.post("http://localhost:7000/api/v1/booking/add", data)
+     if (a) {
+      navigate('/landing')
+   } else {
+      setError(a)
+    }
+  }
 
   return (
     <div id='booking' className='section bg-dark'>
@@ -16,20 +41,27 @@ const Booking = () => {
               <div className='form-header rounded-top'>
                 <h1>Book Your Apppointment</h1>
               </div>
-              <form>
+              <form onSubmit={onAddBooking}>
                 <div className='form-group'>
                   <span className='form-label'>Email</span>
-                  <input className='form-control' type='text' />
+                  <input className='form-control' type='text' 
+                     name='email'
+                      value={data.email}
+                      onChange={onInputChange} />
                 </div>
                 <div className='form-group'>
                   <span className='form-label'>Phone Number</span>
-                  <input className='form-control' type='text' />
+                  <input className='form-control' type='text'  name='phone'
+                      value={data.phone}
+                      onChange={onInputChange} />
                 </div>
                 <div className='row'>
                   <div className='col-md-12'>
                     <div className='form-group'>
                       <span className='form-label'>Appointment Date</span>
-                      <input className='form-control' type='date' />
+                      <input className='form-control' type='date'  name='appointmentDate'
+                      value={data.appointmentDate}
+                      onChange={onInputChange}/>
                     </div>
                   </div>
                 </div>
@@ -40,30 +72,32 @@ const Booking = () => {
                       Select appointment type <span>(Choose atleast one)</span>
                     </p>
 
-                    <label htmlFor='nin'>
+                    <label htmlFor='NIN'>
                       <input
                         className='form-check-input'
                         type='checkbox'
-                        value='nin'
                         id='nin'
-                        name='app-type'
+                        name='service_bvn'
+                        value={data.service}
+                        onChange={onInputChange}
                       />
                       <span></span>NIN
                     </label>
-                    <label htmlFor='bvn'>
+                    <label htmlFor='BVN'>
                       <input
                         className='form-check-input'
                         type='checkbox'
-                        value='bvn'
                         id='bvn'
-                        name='app-type'
+                        name='service_bvn'
+                        value={data.service}
+                        onChange={onInputChange}
                       />
                       <span></span>BVN
                     </label>
                   </div>
                 </div>
                 <div className='form-btn'>
-                  <button className='btn btn-primary'>Book Appointment</button>
+                  <button className='btn btn-primary'  type='submit'>Book Appointment</button>
                 </div>
               </form>
             </div>
